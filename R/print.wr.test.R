95b0c798e2f7031f0a49974042036fc5d55af161
daewoopak <89290503+daewoopak@users.noreply.github.com>
2025-04-30 18:32:12 +0900

    Update print.wr.test.R

--- R/print.wr.test.R
+++ R/print.wr.test.R
@@ -62,9 +62,13 @@ print.wr.test <- function(x, ...) {
   new_wrlin_ar_f <- sprintf(digit, new_wrlin_ar)
   new_pwrlin_ar_f <- display_p(sprintf(digitp, new_pwrlin_ar))
 
-  new_stat_wr0 <- x$wrtest0$stat_wr
+
+  new_wr0 <- x$wrtest0$wr
+  new_ci_wr0 <- x$wrtest0$ci_wr
+  new_zv_wr0 <- x$wrtest0$zv_wr
   new_pval_wr0 <- x$wrtest0$pval_wr
-  new_stat_wr0_f <- sprintf(digit, new_stat_wr0)
+  new_wr0_f <- sprintf(digit, new_wr0)
+  new_zv_wr0_f <- sprintf(digit, new_zv_wr0)
   new_pval_wr0_f <- display_p(sprintf(digitp, new_pval_wr0))
 
 
@@ -86,29 +90,28 @@ print.wr.test <- function(x, ...) {
 
   cat("II. ------------- < Tests of the Global Null Hypothesis > -----------------------\n")
 
-  cat(sprintf("\n>> Linear combination test [l = (%.1f, %.1f)]\n", lin[1], lin[2]))
+  cat(sprintf(">> Linear combination test [l = (%.1f, %.1f)]\n", lin[1], lin[2]))
   cat(sprintf("  [%-5s]  statistic = %s,  p-value %s\n", "ESWR", new_wrlinf[1], new_pvalinallf[1]))
   cat(sprintf("  [%-5s]  statistic = %s,  p-value %s\n", "LRGRE", new_wrlinf[2], new_pvalinallf[2]))
   cat(sprintf("  [%-5s]  statistic = %s,  p-value %s\n", "ICH", new_wrlinf[3], new_pvalinallf[3]))
   cat(sprintf("  [%-5s]  statistic = %s,  p-value %s\n", "ITCH", new_wrlinf[4], new_pvalinallf[4]))
 
-  cat(sprintf("\n>> Linear combination test with data-driven weights* [l = (%.2f, %.2f)]\n", lin_ar[1], lin_ar[2]))
-  cat("*(log transformation used)\n")
+  cat(sprintf("\n>> Linear combination test with data-driven weights [l = (%.2f, %.2f)]\n", lin_ar[1], lin_ar[2]))
   cat(sprintf("  [%-5s]  statistic = %s,  p-value %s\n", "ESWR", new_wrlinf[1], new_pvalinallf[1]))
 
-  cat("\n>> Maximum test (log-log transformation used)\n")
+  cat("\n>> Maximum test\n")
   cat(sprintf("  [%-5s]  statistic = %s,  p-value %s\n", "ESWR", new_mxf[1], new_pvalmxf[1]))
   cat(sprintf("  [%-5s]  statistic = %s,  p-value %s\n", "LRGRE", new_mxf[2], new_pvalmxf[2]))
   cat(sprintf("  [%-5s]  statistic = %s,  p-value %s\n", "ICH", new_mxf[3], new_pvalmxf[3]))
   cat(sprintf("  [%-5s]  statistic = %s,  p-value %s\n", "ITCH", new_mxf[4], new_pvalmxf[4]))
 
   cat(paste("\n>> Chi-squred test: statistic = ", chi, ", p-value ", pvachi, sep = ""), "\n")
-  cat(paste(">> Win ratio test: Chisq.statistic = ", new_stat_wr0_f, ", p-value ", new_pval_wr0_f, sep = ""), "\n")
+  cat(paste(">> Win ratio test: statistic = ", new_zv_wr0_f, ", p-value ", new_pval_wr0_f, sep = ""), "\n")
   cat(paste(">> Log-rank test using time to first event: statistic = ", logrank_statf, ", p-value ", logrank_pvalf, sep = ""), "\n")
   cat(strrep("-", len), "\n\n")
 
-  cat(paste("III. ----------- < Confidence Intervals (", ci_level, "% CI*) > -----------------------------", sep = ""), "\n")
-  cat("*(loglog transformation used)\n\n")
+  cat(paste("III. ----------- < Confidence Intervals (", ci_level, "% CI) > -----------------------------", sep = ""), "\n")
+
   cat(">> Non-terminal event\n")
   cat(sprintf("  [%-5s]  estimate = %.3f,  CI = (%.3f, %.3f)\n", "ESWR", new_wralla[1, 1], new_ci1[3, 1], new_ci1[3, 2]))
   cat(sprintf("  [%-5s]  estimate = %.3f,  CI = (%.3f, %.3f)\n", "LRGRE", new_wralla[1, 2], new_ci1[4, 1], new_ci1[4, 2]))
@@ -130,9 +133,12 @@ print.wr.test <- function(x, ...) {
   cat(sprintf("\n>> Data-driven combinations [l = (%.2f, %.2f)]\n", lin_ar[1], lin_ar[2]))
   cat(sprintf("  [%-5s]  estimate = %.3f,  CI = (%.3f, %.3f)\n", "ESWR",
               as.numeric(wrlinl), as.numeric(cilint[1]), as.numeric(cilint[2])))
+  cat("\n>> Win ratio\n")
+  cat(sprintf("  [%-5s]  estimate = %.3f,  CI = (%.3f, %.3f)\n", "Lose/Win", new_wr0[1], new_ci_wr0[1], new_ci_wr0[2]))
+
   cat(strrep("-", len), "\n\n")
 
-   cat("IV. ------------ < Test of Proportional Hazards > -------------------------------\n")
+  cat("IV. ------------ < Test of Proportional Hazards > -------------------------------\n")
   cat(paste(">> test.stat = ", mxph, ", p-value ", pvalph, sep = ""), "\n")
   cat(strrep("-", len), "\n\n")
 
